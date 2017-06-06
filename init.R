09cbddf443d17cf2b7f67423ab53e3a8da8de02d
andreblgithub <andrewbl@icloud.com>
2017-06-06 15:35:02 +0000

    more stuff

--- init.R
+++ init.R
@@ -3,7 +3,7 @@
 # Example R code to install packages if not already installed
 #
 
-my_packages = c("shiny", "ggplot2")
+my_packages = c("shiny", "ggplot2", "rsconnect")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
